16c806eb336793414a2db99e6472dd751592c16d
Juan Carlos Flores <80058987+JcarlosFAllende@users.noreply.github.com>
2023-08-10 14:58:22 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,62 +13,6 @@ provider "google" {
   region  = "us-central1"
   //credentials = "./curso-ccp-2023-ffd8b389c06c.json"
 }
-resource "google_compute_instance" "instance-1" {
-  boot_disk {
-    auto_delete = true
-    device_name = "instance-1"
-
-    initialize_params {
-      image = "projects/debian-cloud/global/images/debian-11-bullseye-v20230711"
-      size  = 10
-      type  = "pd-balanced"
-    }
-
-    mode = "READ_WRITE"
-  }
-
-  can_ip_forward      = false
-  deletion_protection = false
-  enable_display      = false
-
-  labels = {
-    goog-ec-src = "vm_add-tf"
-  }
-
-  machine_type = "e2-medium"
-  name         = "instance-1"
-
-  network_interface {
-    access_config {
-      network_tier = "PREMIUM"
-    }
-
-    subnetwork = "projects/infrastructure-fr587/regions/us-central1/subnetworks/default"
-  }
-
-  scheduling {
-    automatic_restart   = true
-    on_host_maintenance = "MIGRATE"
-    preemptible         = false
-    provisioning_model  = "STANDARD"
-  }
-
-  service_account {
-    email  = "project-service-account@infrastructure-fr587.iam.gserviceaccount.com"
-    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
-  }
-
-  shielded_instance_config {
-    enable_integrity_monitoring = true
-    enable_secure_boot          = false
-    enable_vtpm                 = true
-  }
-
-  tags = ["https-server"]
-  zone = "us-central1-a"
-}
-# This code is compatible with Terraform 4.25.0 and versions that are backwards compatible to 4.25.0.
-# For information about validating this Terraform code, see https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/google-cloud-platform-build#format-and-validate-the-configuration
 
 resource "google_compute_instance" "instance-1" {
   boot_disk {
